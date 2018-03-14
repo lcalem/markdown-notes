257487bc75115951e74fda162d303b91638b4a04
lcalem <laura.calem@gmail.com>
2018-03-14 19:21:15 +0100

    rendering test markdown file

--- app/server.js
+++ app/server.js
@@ -1,16 +1,38 @@
-var express = require("express");
+const express = require("express");
+const md = require('markdown-it')({
+  html:         true,
+  breaks:       true,
+  linkify:      true,
+  typographer:  false,
+  quotes: '“”‘’'
+})
+            .use(require('markdown-it-mathjax')());
+const fs = require('fs');
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.static('../notes/test'));
 
 var demo = require('./demo3');
 
 // main page
 app.get("/", function(req, res) {
+  var path = '/notes/test/test.md';
+  var file = fs.readFileSync(path, 'utf8');
+  rendered_md = md.render(file.toString());
+  res.render('index', {
+      demo_content: rendered_md
+  });
+
+});
+
+app.get("/demo", function(req, res) {
   result = demo.markdown_demo();
   res.render('index', {
-        demo_content: result
-    });
+      demo_content: result
+  });
 });
 
 var port = process.env.PORT || 3000;
--- notes/test/test.md
+++ notes/test/test.md
@@ -6,6 +6,7 @@
 You can write down your notes here
 Here is a [link](https://markdown-it.github.io/) to a demo of markdown-it
 
+And here is a local cat picture
 ![local image](images/cat_picture.jpg)
 
 ---
