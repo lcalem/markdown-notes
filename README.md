9dbe16d6a609863f0559d7209a51211f93ef86b4
lcalem <laura.calem@gmail.com>
2018-03-19 10:51:24 +0100

    index of notes files

--- README.md
+++ README.md
@@ -11,13 +11,19 @@ Useful when your notes contain images or mathematical stuff<br/>
 Basic usage for local use:
 
 1 - build the docker with `make docker` <br/>
-2 - up it with `make up_server <br/>
-3 - open your browser with localhost:3000 <br/>
+2 - up it with `make up_server` <br/>
+3 - open your browser at `localhost:3000` <br/>
 
 ---
 
 To use an external notes folder<br/>
 Mount whatever volume you like in docker/docker-compose.yml
+
+---
+
+Current limitations:<br/>
+
+- You can't have a note file containing a dash (-)
 
 ---
 
--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const fs = require('fs');
 const md = require('markdown-it')({
   html:         true,
   breaks:       true,
   linkify:      true,
   typographer:  false,
   quotes: '“”‘’'
-})
-            .use(require('markdown-it-mathjax')());
-const fs = require('fs');
+}).use(require('markdown-it-mathjax')());
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +19,55 @@ var demo = require('./demo3');
 
 // main page
 app.get("/", function(req, res) {
-  var path = '/notes/test/test.md';
+  var path = '/notes/'
+
+  var walkSync = function(dir, filelist) {
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (file.startsWith('.')) {
+        return;
+      }
+      if (fs.statSync(dir + file).isDirectory()) {
+        filelist = walkSync(dir + file + '/', filelist);
+      }
+      else if (file.endsWith('.md')) {
+        filelist.push(dir + file);
+      }
+    });
+    return filelist;
+  };
+
+  var file_paths = walkSync(path, []);
+
+  var links = [];
+  file_paths.forEach(function(path) {
+    var url = path.replace('/notes/', '').replace('/', '-').replace('.md', '');
+    var name = '';
+
+    // this is probably seriously unoptimized
+    var parts = url.split('-')
+    for (var i=0; i<parts.length; i++) {
+      if (i < parts.length - 1) {
+        name += '[' + parts[i] + ']';
+      }
+      else {
+        name += ' ' + parts[i]
+      }
+    }
+    links.push({'url': url, 'name': name})
+  });
+
+  // index page rendering with all paths
+  res.render('indexfiles', {
+    files: links
+  })
+
+});
+
+app.get("/notes/:filename", function(req, res) {
+  var path = '/notes/' + req.params.filename.replace("-", "/") + '.md';
   var file = fs.readFileSync(path, 'utf8');
   rendered_md = md.render(file.toString());
   res.render('index', {
@@ -28,8 +76,8 @@ app.get("/", function(req, res) {
 
 });
 
-app.get("/notes/:filename", function(req, res) {
-  var path = '/notes/' + req.params.filename.replace("-", "/") + '.md';
+app.get("/test", function(req, res) {
+  var path = '/notes/test/test.md';
   var file = fs.readFileSync(path, 'utf8');
   rendered_md = md.render(file.toString());
   res.render('index', {
