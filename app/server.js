b279f4b1a2b588ca29e286014774afbdc7f14521
lcalem <laura.calem@gmail.com>
2018-05-30 14:09:55 +0200

    added category shortcuts

--- app/server.js
+++ app/server.js
@@ -22,30 +22,31 @@ app.use(express.static(__dirname + '/notes/'));
 
 var demo = require('./demo3');
 
-// main page
-app.get("/", function(req, res) {
-  var path = '/notes/'
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  var toplevel = [];
+  files.forEach(function(file) {
+    if (file.startsWith('.')) {
+      return;
+    }
+    if (fs.statSync(dir + file).isDirectory()) {
+      toplevel.push(file);
+      filelist = walkSync(dir + file + '/', filelist)["filelist"];
+    }
+    else if (file.endsWith('.md')) {
+      filelist.push(dir + file);
+    }
+  });
 
-  var walkSync = function(dir, filelist) {
-    var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.forEach(function(file) {
-      if (file.startsWith('.')) {
-        return;
-      }
-      if (fs.statSync(dir + file).isDirectory()) {
-        filelist = walkSync(dir + file + '/', filelist);
-      }
-      else if (file.endsWith('.md')) {
-        filelist.push(dir + file);
-      }
-    });
-    return filelist;
+  return {
+    "filelist": filelist,
+    "toplevel": toplevel
   };
+};
 
-  var file_paths = walkSync(path, []);
-
+var linksFromPaths = function(file_paths) {
   var links = [];
   file_paths.forEach(function(path) {
     var url = path.replace('/notes/', '').split('/').join('-').replace('.md', '');
@@ -63,14 +64,32 @@ app.get("/", function(req, res) {
     }
     links.push({'url': url, 'name': name})
   });
+  return links;
+};
+
+// main page
+app.get("/", function(req, res) {
+  var path = '/notes/'
+
+  var paths = walkSync(path, []);
+  var file_paths = paths["filelist"];
+  var toplevel = paths["toplevel"];
+
+  var links = linksFromPaths(file_paths);
+
+  console.log("blblblblbl");
+  console.log(toplevel);
 
   // index page rendering with all paths
   res.render('indexfiles', {
-    files: links
+    files: links,
+    toplevel: toplevel,
+    title: "Notes!"
   })
 
 });
 
+// specific page
 app.get("/notes/:filename", function(req, res) {
   var path = '/notes/' + req.params.filename.split('-').join('/') + '.md';
   var file = fs.readFileSync(path, 'utf8');
@@ -79,6 +98,19 @@ app.get("/notes/:filename", function(req, res) {
       demo_content: rendered_md
   });
 
+});
+
+// category page
+app.get("/notes/type/:folder", function(req, res) {
+  var path = '/notes/' + req.params.folder + '/';
+  var file_paths = walkSync(path, [])["filelist"];
+  var links = linksFromPaths(file_paths);
+
+  res.render('indexfiles', {
+    files: links,
+    toplevel: undefined,
+    title: req.params.folder
+  })
 });
 
 app.get("/test", function(req, res) {
